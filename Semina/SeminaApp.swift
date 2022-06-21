63633d7849fc46e03971c43b950071bacb8a1ef9
“DalioKim” <ejxlabvlzhxm@naver.com>
2022-06-21 13:18:56 +0900

    Initial Commit

---
+++ Semina/SeminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeminaApp.swift
+//  Semina
+//
+//  Created by 김동현 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SeminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
